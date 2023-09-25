7dde0f022e7f5fd86b24fc7801946eee5d491584
Abdullah Zafeer <107130743+Zafeer46@users.noreply.github.com>
2023-09-26 02:44:52 +0500

    50%

--- test/index.php
+++ test/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="index2.css">
     <title>Assignment-01</title>
 </head>
 <body>
@@ -12,12 +12,12 @@
             <h1 class="heading">Hello!</h1>
         </div>
         <div class="form">
-            <form target="_self" action="index2.php">
+            <form target="_self" action="index3.php">
   
                 <input class="username" type="email" placeholder="Enter email" required name="User-name:"> <br>
                 <input class="password" type="password" placeholder="•••••••••••" name="pass:" required> <br>
                 <input class="login" type="submit" value="Log In">
-                <a href="#" class="fpass">Forgot Password?</a>
+                <button class="signup-btn" onclick="document.location='index5.php'" >Signup</button>
   
             </form>
         </div>
