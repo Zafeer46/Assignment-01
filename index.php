b5351f0e2ec6dceaa484b462bdc77b7b42d37906
Abdullah Zafeer <107130743+Zafeer46@users.noreply.github.com>
2023-09-26 01:11:58 +0500

    20% done

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Assignment-01</title>
+</head>
+<body>
+    <section class="total">
+        <div class="head">
+            <h1 class="heading">Hello!</h1>
+        </div>
+        <div class="form">
+            <form target="_self" action="index2.php">
+  
+                <input class="username" type="email" placeholder="Enter email" required name="User-name:"> <br>
+                <input class="password" type="password" placeholder="•••••••••••" name="pass:" required> <br>
+                <input class="login" type="submit" value="Log In">
+                <a href="#" class="fpass">Forgot Password?</a>
+  
+            </form>
+        </div>
+    </section>
+</body>
+</html>
+
+<?php
+
+?>
---
+++ index2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main2.css">
+    <title>Assignment-01</title>
+</head>
+<body>
+    <section class="box">
+        <div class="centerbox">
+            <h1 class="title">Welcome <3</h1>
+        </div>
+        <form action="index.php">
+            <input class="button" type="submit" value="Log Out">
+        </form>
+    </section>
+</body>
+</html>
